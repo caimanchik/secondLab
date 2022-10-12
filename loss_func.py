57a6ea23e351abddc3d301128dd3edc2e38ff74f
Kirill Wolkow <79734984+caimanchik@users.noreply.github.com>
2022-10-12 13:14:37 +0500

    Add files via upload

---
+++ loss_func.py
@@ -0,0 +1,58 @@
+import gspread
+import numpy as np
+
+x = [3, 21, 22, 34, 54, 34, 55, 67, 89, 99]
+x = np.array(x)
+y = [2, 22, 24, 65, 79, 82, 55, 130, 150, 199]
+y = np.array(y)
+
+
+def model(a, b, x):
+    return a * x + b
+
+
+def loss_function(a, b, x, y):
+    num = len(x)
+    prediction = model(a, b, x)
+    return (0.5 / num) * (np.square(prediction - y)).sum()
+
+
+def optimize(a, b, x, y):
+    num = len(x)
+    prediction = model(a, b, x)
+
+    da = (1.0 / num) * ((prediction - y) * x).sum()
+    db = (1.0 / num) * ((prediction - y).sum())
+    a = a - Lr * da
+    b = b - Lr * db
+    return a, b
+
+
+def iterate(a, b, x, y, times):
+    for i in range(times):
+        a, b = optimize(a, b, x, y)
+    return a, b
+
+
+a = np.random.rand(1)
+b = np.random.rand(1)
+Lr = 0.000001
+
+gc = gspread.service_account(filename="unity-caimanchic-91c5ae548f7e.json")
+sh = gc.open("UnitySheets")
+mon = list(range(1, 11))
+
+
+i = 0
+while i <= len(mon):
+    i += 1
+    if i == 0:
+        continue
+    else:
+        a, b = iterate(a, b, x, y, 150)
+        loss = loss_function(a, b, x, y)
+
+        temp_inf = str(loss).replace('.', ',')
+        sh.sheet1.update(('A' + str(i)), str(i))
+        sh.sheet1.update(('B' + str(i)), temp_inf)
+        print(temp_inf)
